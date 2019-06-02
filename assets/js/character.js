f0b7881a78bdcd61d4f4a3396d6e5100d5c196d6
youyou <leigned@outlook.com>
2019-06-02 10:35:43 +0800

    修改

--- assets/js/character.js
+++ assets/js/character.js
@@ -1,33 +1,34 @@
 cc.Class({
-	extends: cc.Component,
+  extends: cc.Component,
 
-	properties: {
-		sprite: cc.Sprite
-	},
+  properties: {
+    sprite: cc.Sprite
+  },
 
-	// LIFE-CYCLE CALLBACKS:
+  // LIFE-CYCLE CALLBACKS:
 
-	// onLoad () {},
-	init(g, data) {
-		this._game = g
-		this.data = data
-		this.isHave = g.PD.characters.some(item => {
-			item == data.number
-		})
-	},
-	start() {
+  // onLoad () {},
+  init(g, data) {
+    this._game = g
+    this.data = data
+    this.isHave = g.PD.characters.some(item => {
+      item == data.number
+    })
+    this.sprite.spriteFrame = g.characterSF[this.data.number]
+  },
+  start() {
 
-	},
-	closeChoose() {
-		this.getChildByName('choose').active = false
-		this.getChildByName('choosebg').active = false
-	},
-	openChoose() {
-		this.getChildByName('choose').active = true
-		this.getChildByName('choosebg').active = true
-	},
-	chooseCharacter() {
-		this.game.switchCharacter(this.data.number)
-	},
-	// update (dt) {},
+  },
+  closeChoose() {
+    this.getChildByName('choose').active = false
+    this.getChildByName('choosebg').active = false
+  },
+  openChoose() {
+    this.getChildByName('choose').active = true
+    this.getChildByName('choosebg').active = true
+  },
+  chooseCharacter() {
+    this._game.switchCharacter(this.data.number, this.sprite.spriteFrame)
+  },
+  // update (dt) {},
 });
